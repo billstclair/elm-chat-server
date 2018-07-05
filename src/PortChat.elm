b0bdfbf022f96bffec0852d5a81071d886b54577
Bill St. Clair <billstclair@gmail.com>
2018-07-05 06:53:40 -0400

    Add copyright headers to new files.

--- src/PortChat.elm
+++ src/PortChat.elm
@@ -1,3 +1,15 @@
+----------------------------------------------------------------------
+--
+-- PortChat.elm
+-- The top-level chat program, using LocalStorage ports.
+-- Copyright (c) 2018 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+
 port module PortChat exposing (..)
 
 import ChatClient.SharedUI exposing (Msg, programWithFlags)
--- src/ReactorChat.elm
+++ src/ReactorChat.elm
@@ -1,3 +1,15 @@
+----------------------------------------------------------------------
+--
+-- ReactorChat.elm
+-- The top-level chat program, using simulated LocalStorage ports.
+-- Copyright (c) 2018 Bill St. Clair <billstclair@gmail.com>
+-- Some rights reserved.
+-- Distributed under the MIT License
+-- See LICENSE.txt
+--
+----------------------------------------------------------------------
+
+
 module ReactorChat exposing (..)
 
 import ChatClient.SharedUI
